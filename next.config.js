296e5dc12e931422eaaa40ece8e78dfbe841ffb1
CodeDoctor <wusel905@gmail.com>
2020-12-30 09:51:05 +0100

    add action

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
-    runtimeCaching,
+    dest: 'public'
   },
 })
