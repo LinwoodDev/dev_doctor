d20f38064093532d14376cab7db25ace9d4f1be0
CodeDoctor <wusel905@gmail.com>
2020-12-30 13:36:42 +0100

    improve pwa warnings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <html lang="en">
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
@@ -32,6 +32,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta name="theme-color" content="#317EFB" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </html>
   )
 }
