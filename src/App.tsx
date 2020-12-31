1ee8540e1cb831eb760069f2ddfb660aea292d2b
CodeDoctor <wusel905@gmail.com>
2020-12-31 10:27:28 +0100

    add appbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,22 @@ import './App.css';
 import theme from './theme';
 import { ThemeProvider } from '@material-ui/core';
 import IndexPage from './pages/index';
-import { Route, 
-    BrowserRouter as Router, Switch } from 'react-router-dom';
+import {
+  Route,
+  BrowserRouter as Router, Switch
+} from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import MyAppBar from './components/appbar';
 
 function App() {
-    const { i18n } = useTranslation();
-    const changeLanguage = (lng : string) => {
-      (i18n as any).changeLanguage(lng);
-    }
   return (
     <ThemeProvider theme={theme}>
-        <button onClick={() => changeLanguage('de')}>de</button>
-        <button onClick={() => changeLanguage('en')}>en</button>
-        <Router>
-            <Switch>
-                <Route path="/" component={IndexPage} />
-            </Switch>
-        </Router>
+      <MyAppBar />
+      <Router>
+        <Switch>
+          <Route path="/" component={IndexPage} />
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
