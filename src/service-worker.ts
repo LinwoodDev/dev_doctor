7c6eeb4c6fc76b0e8250ad4c9e7f48fc52091098
CodeDoctor <wusel905@gmail.com>
2020-12-31 16:13:34 +0100

    fix service worker offline data

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -78,3 +78,11 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
+  );
+});
