cd3b614c8048ee8905b83510500c48a03dac2969
CodeDoctor <wusel905@gmail.com>
2020-12-31 10:22:51 +0100

    add updater

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import './i18next';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import ServiceWorkerWrapper from './serviceWorkerWrapper';
 
 ReactDOM.render(
-  <Suspense fallback="loading">
-    <App />
-  </Suspense>,
+    <Suspense fallback="loading">
+    <App />,
+    <ServiceWorkerWrapper />
+    </Suspense>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
