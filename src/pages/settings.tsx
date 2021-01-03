18f0c7eb37db11db7ac430b8b0d449afb8ec82a6
CodeDoctor <wusel905@gmail.com>
2021-01-03 13:48:08 +0100

    remove warning

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -2,11 +2,7 @@ import React, { ReactElement } from 'react'
 import { useTranslation } from "react-i18next";
 import MyAppBar from '../components/appbar'
 
-interface Props {
-    
-}
-
-export default function SettingsPage({}: Props): ReactElement {
+export default function SettingsPage(): ReactElement {
     const {t} = useTranslation("settings")
     return (
         <>
