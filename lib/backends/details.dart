7c0639240cdf521023003cfafd13464bc3ed96a2
CodeDoctorDE <wusel905@gmail.com>
2021-02-17 14:34:32 +0100

    add heros to backend

--- lib/backends/details.dart
+++ lib/backends/details.dart
@@ -33,68 +33,71 @@ class _BackendPageState extends State<BackendPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: FutureBuilder<CoursesServer>(
-            future: _buildFuture(),
-            builder: (context, snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.waiting:
-                  return Center(child: CircularProgressIndicator());
-                default:
-                  if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-                  var server = snapshot.data;
-                  return NestedScrollView(
-                      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-                        return <Widget>[
-                          SliverAppBar(
-                            expandedHeight: 400.0,
-                            floating: false,
-                            pinned: true,
-                            actions: [AddBackendButton(server: server)],
-                            flexibleSpace: FlexibleSpaceBar(
-                              centerTitle: true,
-                              title: Container(
-                                  padding: EdgeInsets.all(5),
-                                  decoration: BoxDecoration(
-                                    color: Theme.of(context).primaryColor.withAlpha(200),
-                                    borderRadius: BorderRadius.circular(5),
-                                  ),
-                                  child: Text(server.name)),
-                              background: Container(
-                                  margin: EdgeInsets.fromLTRB(10, 20, 10, 84),
-                                  child: UniversalImage(
-                                    url: server.url + "/icon",
-                                    height: 500,
-                                    type: server.icon,
-                                  )),
-                            ),
-                          )
-                        ];
-                      },
-                      body: ListView(
-                        children: <Widget>[
-                          if (server.body != null)
-                            Padding(
-                                padding: EdgeInsets.all(16),
-                                child: Card(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(16)),
-                                    child: Padding(
-                                        padding: const EdgeInsets.all(64.0),
-                                        child: MarkdownBody(
-                                          onTapLink: (_, url, __) => launch(url),
-                                          extensionSet: md.ExtensionSet(
-                                            md.ExtensionSet.gitHubFlavored.blockSyntaxes,
-                                            [
-                                              md.EmojiSyntax(),
-                                              ...md.ExtensionSet.gitHubFlavored.inlineSyntaxes
-                                            ],
-                                          ),
-                                          data: server.body,
-                                          selectable: true,
-                                        )))),
-                        ],
-                      ));
-              }
-            }));
+        body: widget.model != null
+            ? _buildView(widget.model)
+            : FutureBuilder<CoursesServer>(
+                future: _buildFuture(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return Center(child: CircularProgressIndicator());
+                    default:
+                      if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+                      var server = snapshot.data;
+                      return _buildView(server);
+                  }
+                }));
   }
+
+  Widget _buildView(CoursesServer server) => NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return <Widget>[
+          SliverAppBar(
+            expandedHeight: 400.0,
+            floating: false,
+            pinned: true,
+            actions: [AddBackendButton(server: server)],
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor.withAlpha(200),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(server.name)),
+              background: Container(
+                  margin: EdgeInsets.fromLTRB(10, 20, 10, 84),
+                  child: Hero(
+                      tag:
+                          "backend-icon-${server.entry.collection.index}-${server.entry.user}-${server.entry.name}",
+                      child: UniversalImage(
+                        url: server.url + "/icon",
+                        height: 500,
+                        type: server.icon,
+                      ))),
+            ),
+          )
+        ];
+      },
+      body: ListView(
+        children: <Widget>[
+          if (server.body != null)
+            Padding(
+                padding: EdgeInsets.all(16),
+                child: Card(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                    child: Padding(
+                        padding: const EdgeInsets.all(64.0),
+                        child: MarkdownBody(
+                          onTapLink: (_, url, __) => launch(url),
+                          extensionSet: md.ExtensionSet(
+                            md.ExtensionSet.gitHubFlavored.blockSyntaxes,
+                            [md.EmojiSyntax(), ...md.ExtensionSet.gitHubFlavored.inlineSyntaxes],
+                          ),
+                          data: server.body,
+                          selectable: true,
+                        )))),
+        ],
+      ));
 }
--- lib/courses/course.dart
+++ lib/courses/course.dart
@@ -30,114 +30,108 @@ class _CoursePageState extends State<CoursePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: FutureBuilder<Course>(
-            future: _buildFuture(),
-            builder: (context, snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.waiting:
-                  return Center(child: CircularProgressIndicator());
-                default:
-                  if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-                  var course = snapshot.data;
-                  return NestedScrollView(
-                      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-                        return <Widget>[
-                          SliverAppBar(
-                            expandedHeight: 400.0,
-                            floating: false,
-                            pinned: true,
-                            actions: [
-                              IconButton(
-                                icon: Icon(Icons.play_circle_outline_outlined),
-                                tooltip: "course.start".tr(),
-                                onPressed: () => Modular.to.navigate(
-                                    '/courses/start/item?serverId=${widget.serverId}&courseId=${widget.courseId}&partId=0'),
-                              )
-                            ],
-                            flexibleSpace: FlexibleSpaceBar(
-                              centerTitle: true,
-                              title: Container(
-                                  padding: EdgeInsets.all(5),
-                                  decoration: BoxDecoration(
-                                    color: Theme.of(context).primaryColor.withAlpha(200),
-                                    borderRadius: BorderRadius.circular(5),
-                                  ),
-                                  child: Text(course.name)),
-                              background: Container(
-                                  margin: EdgeInsets.fromLTRB(10, 20, 10, 84),
-                                  child: UniversalImage(
-                                    url: course.url + "/icon",
-                                    height: 500,
-                                    type: course.icon,
-                                  )),
-                            ),
-                          )
-                        ];
-                      },
-                      body: ListView(
-                        children: <Widget>[
-                          Padding(
-                              padding: EdgeInsets.all(4),
-                              child: Card(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(16)),
-                                  child: Padding(
-                                      padding: EdgeInsets.all(16),
-                                      child: Column(children: [
-                                        GestureDetector(
-                                            onTap: () {
-                                              if (course.authorUrl != null)
-                                                launch(course.authorUrl);
-                                            },
-                                            child: Row(
-                                                mainAxisAlignment: MainAxisAlignment.center,
-                                                children: [
-                                                  if (course.authorAvatar != null)
-                                                    Padding(
-                                                        padding: EdgeInsets.all(8),
-                                                        child: CircleAvatar(
-                                                          child: ClipOval(
-                                                            child:
-                                                                Image.network(course.authorAvatar),
-                                                          ),
-                                                        )),
-                                                  Text(course.author ?? "")
-                                                ])),
-                                        if (course.lang != null)
-                                          Padding(
-                                              padding: EdgeInsets.all(8),
-                                              child: Row(
-                                                  mainAxisAlignment: MainAxisAlignment.center,
-                                                  children: [
-                                                    Padding(
-                                                        padding: EdgeInsets.all(4),
-                                                        child: Icon(Icons.language_outlined)),
-                                                    Text(
-                                                        LocaleNames.of(context).nameOf(course.lang))
-                                                  ]))
-                                      ])))),
-                          Padding(
-                              padding: EdgeInsets.all(16),
-                              child: Card(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(16)),
-                                  child: Padding(
-                                      padding: const EdgeInsets.all(64.0),
-                                      child: MarkdownBody(
-                                        onTapLink: (_, url, __) => launch(url),
-                                        extensionSet: md.ExtensionSet(
-                                          md.ExtensionSet.gitHubFlavored.blockSyntaxes,
-                                          [
-                                            md.EmojiSyntax(),
-                                            ...md.ExtensionSet.gitHubFlavored.inlineSyntaxes
-                                          ],
-                                        ),
-                                        data: course.body,
-                                        selectable: true,
-                                      )))),
-                        ],
-                      ));
-              }
-            }));
+        body: widget.model != null
+            ? _buildView(widget.model)
+            : FutureBuilder<Course>(
+                future: _buildFuture(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return Center(child: CircularProgressIndicator());
+                    default:
+                      if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+                      var course = snapshot.data;
+                      return _buildView(course);
+                  }
+                }));
   }
+
+  Widget _buildView(Course course) => NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return <Widget>[
+          SliverAppBar(
+            expandedHeight: 400.0,
+            floating: false,
+            pinned: true,
+            actions: [
+              IconButton(
+                icon: Icon(Icons.play_circle_outline_outlined),
+                tooltip: "course.start".tr(),
+                onPressed: () => Modular.to.navigate(
+                    '/courses/start/item?serverId=${widget.serverId}&courseId=${widget.courseId}&partId=0'),
+              )
+            ],
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor.withAlpha(200),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(course.name)),
+              background: Container(
+                  margin: EdgeInsets.fromLTRB(10, 20, 10, 84),
+                  child: Hero(
+                      tag: "course-icon-${widget.serverId}-${widget.courseId}",
+                      child: UniversalImage(
+                        url: course.url + "/icon",
+                        height: 500,
+                        type: course.icon,
+                      ))),
+            ),
+          )
+        ];
+      },
+      body: ListView(
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.all(4),
+              child: Card(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                  child: Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Column(children: [
+                        GestureDetector(
+                            onTap: () {
+                              if (course.authorUrl != null) launch(course.authorUrl);
+                            },
+                            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                              if (course.authorAvatar != null)
+                                Padding(
+                                    padding: EdgeInsets.all(8),
+                                    child: CircleAvatar(
+                                      child: ClipOval(
+                                        child: Image.network(course.authorAvatar),
+                                      ),
+                                    )),
+                              Text(course.author ?? "")
+                            ])),
+                        if (course.lang != null)
+                          Padding(
+                              padding: EdgeInsets.all(8),
+                              child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                                Padding(
+                                    padding: EdgeInsets.all(4),
+                                    child: Icon(Icons.language_outlined)),
+                                Text(LocaleNames.of(context).nameOf(course.lang))
+                              ]))
+                      ])))),
+          Padding(
+              padding: EdgeInsets.all(16),
+              child: Card(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                  child: Padding(
+                      padding: const EdgeInsets.all(64.0),
+                      child: MarkdownBody(
+                        onTapLink: (_, url, __) => launch(url),
+                        extensionSet: md.ExtensionSet(
+                          md.ExtensionSet.gitHubFlavored.blockSyntaxes,
+                          [md.EmojiSyntax(), ...md.ExtensionSet.gitHubFlavored.inlineSyntaxes],
+                        ),
+                        data: course.body,
+                        selectable: true,
+                      )))),
+        ],
+      ));
 }
