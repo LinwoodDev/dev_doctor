b8dfc685d8fe27e7d75b7b4a6c4c65f512d40747
CodeDoctorDE <wusel905@gmail.com>
2021-02-20 07:34:05 +0100

    add user page

--- lib/backends/home.dart
+++ lib/backends/home.dart
@@ -18,7 +18,8 @@ class _ItemFetcher {
     if (entries.isEmpty)
       await Future.wait(Hive.box<String>('collections').values.map((e) async {
         var collection = await BackendCollection.fetch(url: e);
-        entries.addAll((await collection.fetchUsers()).expand((e) => e.buildEntries()));
+        var users = await collection.fetchUsers();
+        entries.addAll(users.expand((e) => e.buildEntries()));
       }));
     final list = <CoursesServer>[];
     var n = min(_itemsPerPage, entries.length - _currentPage * _itemsPerPage);
---
+++ lib/backends/user.dart
@@ -0,0 +1,74 @@
+import 'package:dev_doctor/models/collection.dart';
+import 'package:dev_doctor/models/server.dart';
+import 'package:dev_doctor/widgets/image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:markdown/markdown.dart' as md;
+import 'package:url_launcher/url_launcher.dart';
+
+import 'home.dart';
+
+class BackendPage extends StatefulWidget {
+  final String user;
+  final int collectionId;
+  final BackendUser model;
+
+  const BackendPage({Key key, this.user, this.collectionId, this.model}) : super(key: key);
+
+  @override
+  _BackendPageState createState() => _BackendPageState();
+}
+
+class _BackendPageState extends State<BackendPage> {
+  Future<BackendUser> _buildFuture() async {
+    if (widget.model != null) return widget.model;
+    var collection = await BackendCollection.fetch(index: widget.collectionId);
+    var user = await collection.fetchUser(widget.user);
+    return await user;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: widget.model != null
+            ? _buildView(widget.model)
+            : FutureBuilder<BackendUser>(
+                future: _buildFuture(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return Center(child: CircularProgressIndicator());
+                    default:
+                      if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+                      var user = snapshot.data;
+                      return _buildView(user);
+                  }
+                }));
+  }
+
+  Widget _buildView(BackendUser user) => Scaffold(
+      appBar: AppBar(title: Text(user.name)),
+      body: Scrollbar(
+          child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: List.generate(user.entries.length, (index) {
+                return Container(
+                    width: 160.0,
+                    child: FutureBuilder<CoursesServer>(
+                        future: user.buildEntry(user.entries[index]).fetchServer(),
+                        builder: (context, snapshot) {
+                          switch (snapshot.connectionState) {
+                            case ConnectionState.waiting:
+                              return Center(child: CircularProgressIndicator());
+                            default:
+                              if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+                              var server = snapshot.data;
+                              return Column(children: [
+                                UniversalImage(
+                                    url: server.url + "/icon", type: server.icon, width: 160),
+                                Text(server.name)
+                              ]);
+                          }
+                        }));
+              }))));
+}
