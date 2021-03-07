b992a0edc04231bf56c41c0d6382479f0e225e89
CodeDoctorDE <wusel905@gmail.com>
2021-03-07 08:21:10 +0100

    dismissible backends

--- lib/editor/home.dart
+++ lib/editor/home.dart
@@ -6,7 +6,6 @@ import 'package:dev_doctor/editor/create.dart';
 import 'package:dev_doctor/widgets/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -29,7 +28,14 @@ class _EditorPageState extends State<EditorPage> {
                     itemBuilder: (context, index) {
                       var item = json.decode(_box.getAt(index));
                       var bloc = ServerEditorBloc.fromJson(item);
-                      return ListTile(title: Text(bloc.server.name), subtitle: Text(bloc.note));
+                      return Dismissible(
+                          key: Key(_box.name),
+                          background: Container(color: Colors.red),
+                          onDismissed: (direction) {
+                            _box.deleteAt(index);
+                          },
+                          child:
+                              ListTile(title: Text(bloc.server.name), subtitle: Text(bloc.note)));
                     }))),
         floatingActionButton: OpenContainer(
             transitionType: ContainerTransitionType.fadeThrough,
--- lib/editor/module.dart
+++ lib/editor/module.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
-import 'create.dart';
 import 'home.dart';
 
 class EditorModule extends Module {
