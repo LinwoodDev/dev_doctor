befafc897b0b380e8499cf110d197725cf1d7833
CodeDoctorDE <wusel905@gmail.com>
2021-03-09 18:05:17 +0100

    change details page

--- lib/courses/part/details.dart
+++ lib/courses/part/details.dart
@@ -1,13 +1,41 @@
+import 'package:dev_doctor/editor/bloc/server.dart';
+import 'package:dev_doctor/models/part.dart';
+import 'package:dev_doctor/models/server.dart';
+import 'package:dev_doctor/widgets/appbar.dart';
 import 'package:flutter/material.dart';
 
 class PartDetailsPage extends StatefulWidget {
+  final CoursePart model;
+  final int partId;
+  final int courseId;
+  final int serverId;
+  final ServerEditorBloc editorBloc;
+
+  const PartDetailsPage(
+      {Key key, this.model, this.partId, this.courseId, this.serverId, this.editorBloc})
+      : super(key: key);
   @override
   _PartDetailsPageState createState() => _PartDetailsPageState();
 }
 
 class _PartDetailsPageState extends State<PartDetailsPage> {
+  Future<CoursePart> _buildFuture() async {
+    if (widget.model != null) return widget.model;
+    var server = await CoursesServer.fetch(index: widget.serverId);
+    var course = await server.fetchCourse(widget.courseId);
+    return course.fetchPart(widget.partId);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return FutureBuilder<CoursePart>(
+        future: _buildFuture(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting || !snapshot.hasData)
+            return Center(child: CircularProgressIndicator());
+          if (snapshot.hasError) return Text("Error ${snapshot.error}");
+          var part = snapshot.data;
+          return Scaffold(appBar: MyAppBar(title: part.name), body: ListView(children: []));
+        });
   }
 }
