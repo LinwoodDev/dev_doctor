adae34d33f011b5a94efd45dfc7f7e4d090d259c
CodeDoctorDE <wusel905@gmail.com>
2021-03-14 14:58:38 +0100

    editing services

--- lib/editor/bloc/course.dart
+++ lib/editor/bloc/course.dart
@@ -3,16 +3,16 @@ import 'package:dev_doctor/models/course.dart';
 import 'package:dev_doctor/models/part.dart';
 
 class CourseEditorBloc {
-  final ServerEditorBloc serverBloc;
+  final ServerEditorBloc bloc;
   final Course course;
   final List<CoursePart> parts;
 
-  CourseEditorBloc(this.course, {this.serverBloc, List<CoursePart> parts = const []})
+  CourseEditorBloc(this.course, {this.bloc, List<CoursePart> parts = const []})
       : parts = List<CoursePart>.unmodifiable(parts);
 
   CourseEditorBloc.fromJson(Map<String, dynamic> json)
       : course = Course.fromJson(json['course']),
-        serverBloc = json['bloc'],
+        bloc = json['bloc'],
         parts = List<CoursePart>.unmodifiable((json['parts'] as List<dynamic> ?? [])
                 .map((e) => CoursePart.fromJson(e))
                 .toList(growable: false) ??
@@ -20,5 +20,5 @@ class CourseEditorBloc {
   Map<String, dynamic> toJson() =>
       {"course": course.toJson(), "parts": parts.map((e) => e.toJson()).toList()};
   CourseEditorBloc copyWith({Course course, List<CoursePart> parts}) =>
-      CourseEditorBloc(course ?? this.course, serverBloc: serverBloc, parts: parts ?? this.parts);
+      CourseEditorBloc(course ?? this.course, parts: parts ?? this.parts, bloc: bloc);
 }
--- lib/editor/services/course.dart
+++ lib/editor/services/course.dart
@@ -1,4 +1,5 @@
 import 'package:dev_doctor/editor/bloc/course.dart';
+import 'package:dev_doctor/models/course.dart';
 
 import 'server.dart';
 
@@ -16,4 +17,47 @@ class CourseEditorService {
     service.updateCourse(bloc.course.slug, bloc);
     service.save();
   }
+
+  Course get course => bloc.course;
+  /* set course(Course value) => bloc = bloc.copyWith(course: value)..save();
+
+  CoursePart createPart(String name) {
+    if (getCourse(name) != null) return null;
+    var course = CoursePart(Course(name: slug, slug: slug));
+    var courses = List<CourseEditorBloc>.from(bloc.courses);
+    courses.add(course);
+    bloc = bloc.copyWith(courses: courses);
+    save();
+    return course;
+  }
+
+  void updatePart(String slug, CoursePart courseBloc) {
+    var current = List<CourseEditorBloc>.from(courses);
+    current[bloc.server.courses.indexOf(slug)] = courseBloc;
+    bloc = bloc.copyWith(courses: current);
+  }
+
+  CourseEditorBloc getCourseBloc(String slug) {
+    return bloc.courses.firstWhere((e) => e.course.slug == slug);
+  }
+
+  CourseEditorService getCourse(String slug) => CourseEditorService.fromSlug(this, slug);
+
+  void deleteCourse(String slug) {
+    var courses = List<CourseEditorBloc>.from(bloc.courses);
+    courses = courses.where((element) => element.course.slug == slug);
+    bloc = bloc.copyWith(courses: courses);
+    save();
+  }
+
+  void save() {
+    bloc.save();
+  }
+
+  CoursesServer get server => bloc.server;
+  set server(CoursesServer value) => bloc = bloc.copyWith(server: value)..save();
+  String get note => bloc.note;
+  set note(String value) => bloc = bloc.copyWith(note: bloc.note)..save();
+  List<CourseEditorBloc> get courses => bloc.courses;
+  int get key => bloc.key; */
 }
